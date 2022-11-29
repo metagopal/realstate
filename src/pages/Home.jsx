a59cb88c7e94ab30d3f87dfb1de3401cf548552f
Gopal Agarwal <gopal.agarwal@metacube.com>
2022-11-29 16:44:36 +0530

    router  added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="App">
+      <h1 className="text-2xl bg-red-50">lorem 1</h1>
+    </div>
+  );
+}
+
+export default Home;
