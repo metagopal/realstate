3889e5959749d4660ec1f70a43987a14b418ed37
Gopal Agarwal <gopal.agarwal@metacube.com>
2022-12-02 16:47:52 +0530

    create the create listing page UI

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
 import ForgotPassword from "./pages/ForgotPassword";
 import Home from "./pages/Home";
 import Offers from "./pages/Offers";
@@ -28,6 +29,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />}></Route>
           <Route path="/forgot-password" element={<ForgotPassword />}></Route>
           <Route path="/offers" element={<Offers />}></Route>
+          <Route path="/create-listing" element={<CreateListing />}></Route>
         </Routes>
       </Router>
       <ToastContainer
