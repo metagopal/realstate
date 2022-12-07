b9b6951d06f57fea7b10a2ca0ee12b98468947e6
Gopal Agarwal <gopal.agarwal@metacube.com>
2022-12-07 18:30:44 +0530

    ADd the listing information to the listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -3,7 +3,7 @@ import { doc, getDoc } from "firebase/firestore";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase";
 import Spinner from "../components/Spinner";
-import { FaShare } from "react-icons/fa";
+import { FaShare, FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair } from "react-icons/fa";
 
 import localImg from "../assets/key.jpg";
 
@@ -54,6 +54,7 @@ function Listing() {
       setShareLinkCopied(false);
     }, 3000);
   };
+  console.log(listing);
   return (
     <main>
       <Swiper
@@ -90,6 +91,70 @@ function Listing() {
           Link Copied
         </p>
       )}
+
+      <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg  shadow-lg bg-white lg:space-x-5 ">
+        <div className="w-full h-[200px] lg-[400px]">
+          <p className="text-2xl font-bold mb-3 text-blue-900 ">
+            {listing.name} - $
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice
+                  ?.toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {listing.type === "rent" ? "/ month" : ""}
+          </p>
+          <p className="flex items-center mt-6 mb-3 font-semibold">
+            <FaMapMarkerAlt className="text-green-700 mr-1" />
+            {listing.address}
+          </p>
+          <div className="flex justify-start items-center space-x-4 w-[75%]">
+            <p className="bg-red-800 w-full max-w-[200px ] rounded-md p-1 text-white text-center font-semibold shadow-md ">
+              {listing.type === "rent" ? "Rent" : "Sale"}
+            </p>
+            {listing.offer && (
+              <p className="w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white text-center font-semibold shadow-md ">
+                $
+                {+listing.regularPrice -
+                  +listing.discountedPrice}{" "}
+                Discount
+              </p>
+            )}
+          </div>
+          <p className="mt-3 mb-3 ">
+            <span className="font-semibold">Description - </span>
+            {listing.description}
+          </p>
+          <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold">
+            <li className="flex items-center whitespace-nowrap">
+                <FaBed className="text-lg mr-1 " />
+                {
+                    +listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : '1 Bed'
+                }
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+                <FaBath className="text-lg mr-1 " />
+                {
+                    +listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : '1 Bath'
+                }
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+                <FaParking className="text-lg mr-1 " />
+                {
+                    listing.parking ? 'Parking' : ' No parking'
+                }
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+                <FaChair className="text-lg mr-1 " />
+                {
+                    listing.furnished ? 'Furnished' : ' Not furnished'
+                }
+            </li>
+          </ul>
+        </div>
+        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-hidden"></div>
+      </div>
     </main>
   );
 }
