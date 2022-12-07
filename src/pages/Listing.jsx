486683078641a78bef94b269599d6a368c4ed5a0
Gopal Agarwal <gopal.agarwal@metacube.com>
2022-12-07 15:20:43 +0530

    add share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -3,6 +3,7 @@ import { doc, getDoc } from "firebase/firestore";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase";
 import Spinner from "../components/Spinner";
+import { FaShare } from "react-icons/fa";
 
 import localImg from "../assets/key.jpg";
 
@@ -11,13 +12,19 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css/bundle";
-import SwiperCore, { Autoplay, EffectFade, Navigation, Pagination } from "swiper";
+import SwiperCore, {
+  Autoplay,
+  EffectFade,
+  Navigation,
+  Pagination,
+} from "swiper";
 
 function Listing() {
   const params = useParams();
 
   const [listing, setListing] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
   const { imgUrls } = listing;
 
@@ -40,7 +47,13 @@ function Listing() {
     return <Spinner />;
   }
 
-  console.log(listing);
+  const handleCopied = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setShareLinkCopied(true);
+    setTimeout(() => {
+      setShareLinkCopied(false);
+    }, 3000);
+  };
   return (
     <main>
       <Swiper
@@ -58,13 +71,25 @@ function Listing() {
                 className="relative w-full overflow-hidden h-[300px]"
                 style={{
                   background: `url(${listing.imgUrls[index]}) center no-repeat`,
-                  backgroundSize:"cover"
+                  backgroundSize: "cover",
                 }}
               ></div>
             </SwiperSlide>
           );
         })}
       </Swiper>
+
+      <div
+        className="fixed top-[13%] right-[3%] bg-white z-10 cursor-pointer rounded-full w-12 h-12 flex justify-center items-center border-2 border-x-gray-400 "
+        onClick={handleCopied}
+      >
+        <FaShare className="text-lg font-light text-slate-500" />
+      </div>
+      {shareLinkCopied && (
+        <p className=" fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied
+        </p>
+      )}
     </main>
   );
 }
